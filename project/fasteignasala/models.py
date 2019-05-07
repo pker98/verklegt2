58dceb833883333ea681068bbb4f9c109a795896
Matthías Finnur <matthias18@ru.is>
2019-05-07 16:59:17 +0000

    basic models installed

--- project/fasteignasala/models.py
+++ project/fasteignasala/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 # from django.contrib.auth.models import User
 
+class base_user(models.Model):
+    name = models.CharField(max_length=255)
+    SSN = models.CharField(max_length=10)
+
+class Sales_user(models.Model):
+    pass
+
+class Salesman(models.Model):
+    description = models.CharField(max_length=255)
+    user = models.ForeignKey(base_user, on_delete=models.CASCADE)
+
 class Apartment(models.Model):
     description = models.CharField(max_length=255)
-    salesman = models.ForeignKey(User, on_delete=models.SET_NULL)
+    salesman = models.ForeignKey(Sales_user, on_delete=models.SET_NULL)
 
 class ApartmentImage(models.Model):
     image = models.CharField(max_length=999)
