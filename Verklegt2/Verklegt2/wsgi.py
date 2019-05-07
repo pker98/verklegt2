1dfe141d1f8e8a1187fa11a632f8bd8967259a7d
Matthías Finnur <matthias18@ru.is>
2019-05-07 12:42:28 +0000

    Renamed files

--- Verklegt2/Verklegt2/wsgi.py
+++ Verklegt2/Verklegt2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Fasteignasala project.
+WSGI config for Verklegt2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fasteignasala.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Verklegt2.settings')
 
 application = get_wsgi_application()
--- Verklegt2/manage.py
+++ Verklegt2/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fasteignasala.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Verklegt2.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
