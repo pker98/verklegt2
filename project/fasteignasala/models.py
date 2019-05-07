62941bfe0ce7047657810aa83209985bc2c4feef
Matthías Finnur <matthias18@ru.is>
2019-05-07 16:23:36 +0000

    Beginning of database setup

--- project/fasteignasala/models.py
+++ project/fasteignasala/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import User
+
+class Apartment(models.Model):
+    description = models.CharField(max_length=255)
+    salesman = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+class ApartmentImage(models.Model):
+    image = models.CharField(max_length=999)
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
 
 # Create your models here.
