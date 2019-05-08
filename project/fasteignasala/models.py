2ee3265f6cd3a23440eac5e76d5268891246f296
Matthías Finnur <matthias18@ru.is>
2019-05-08 14:55:51 +0000

    more database stuff

--- project/fasteignasala/models.py
+++ project/fasteignasala/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class base_user(models.Model):
+class Base_user(models.Model):
     name = models.CharField(max_length=255)
     SSN = models.CharField(max_length=10)
 
-class Salesman(models.Model):
+class Sales_user(models.Model):
     description = models.CharField(max_length=255)
-    user = models.ForeignKey(base_user, on_delete=models.CASCADE)
+    user = models.ForeignKey(Base_user, on_delete=models.CASCADE)
 
 class Apartment(models.Model):
     description = models.CharField(max_length=255)
-    salesman = models.ForeignKey(Salesman, on_delete=models.CASCADE)
+    salesman = models.ForeignKey(Sales_user, on_delete=models.CASCADE)
 
 class ApartmentImage(models.Model):
     image = models.CharField(max_length=999)
     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
 
-# Create your models here.
+class SalesmanImage(models.Model):
+    image = models.CharField(max_length=999)
+    salesman = models.ForeignKey(Sales_user, on_delete=models.CASCADE)
