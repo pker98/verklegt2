b403922dea0bb11b96fa6ada476c2decd5bcb09d
davidfannar98 <davidfannar98@gmail.com>
2019-05-12 22:27:31 +0000

    login logout

--- project/notandi/urls.py
+++ project/notandi/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('nyskraning/', views.nyskraning, name="nyskraning"),
     path('innskraning/', auth_views.LoginView.as_view(template_name='notandi/innskraning.html'), name="innskraning"),
-    path('utskraning/', auth_views.LogoutView.as_view(), name="utskraning"),
+    path('utskraning/', auth_views.LogoutView.as_view(next_page='innskraning'), name="utskraning"),
 ]
