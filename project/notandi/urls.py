e96a7d3a507b400b8d974dbc7c812b181ca24c87
JonHS97 <42572676+JonHS97@users.noreply.github.com>
2019-05-13 10:23:39 +0000

    Faldi edit/delete takkana
    Faldi edit/delete takkana ef þú ert ekki skráður inn.

--- project/notandi/urls.py
+++ project/notandi/urls.py
@@ -3,8 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('nyskraning/', views.nyskraning, name="nyskraning"),
-    path('innskraning/', auth_views.LoginView.as_view(template_name='notandi/innskraning.html'), name="innskraning"),
-    path('utskraning/', auth_views.LogoutView.as_view(next_page='innskraning'), name="utskraning"),
+    path('nyskraning', views.nyskraning, name="nyskraning"),
+    path('innskraning', auth_views.LoginView.as_view(template_name='notandi/innskraning.html'), name="innskraning"),
+    path('utskraning', auth_views.LogoutView.as_view(next_page='innskraning'), name="utskraning"),
     path('minar_sidur', views.profile, name='minar_sidur')
 ]
