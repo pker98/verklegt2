258e32f740c42719306c552ea23ffe257a22b834
Sunneva <sunnevass@hotmail.com>
2019-05-13 17:50:15 +0000

    almennar upplýsingar fyrst svo greiða

--- project/greidsla/urls.py
+++ project/greidsla/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('greidsluferli/', views.greidsla, name='greidsla')
+    path('greidsluferli/', views.greidsla, name='greidsla'),
+    path('almennar_upplysingar/', views.almupp, name='upplysingar')
 ]
--- project/greidsla/views.py
+++ project/greidsla/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def greidsla(request):
     return render(request, 'greidsluferli/greidsla.html', {"title": "Greiðsluferli"})
 
+def almupp(request):
+    return render(request, 'greidsluferli/upplysingar.html', {"title": "Almennar Upplýsingar"})
