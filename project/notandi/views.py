f1ee513526596e95997026be0530e88afb51c5a4
JonHS97 <42572676+JonHS97@users.noreply.github.com>
2019-05-14 18:36:47 +0000

    added profile info in profile.html

--- project/notandi/views.py
+++ project/notandi/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
