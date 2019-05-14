376da79f6a9c289ef94bd084bc916466aeb74673
Matthías Finnur <matthias18@ru.is>
2019-05-14 15:40:56 +0000

    merge

--- project/fasteignasala/urls.py
+++ project/fasteignasala/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name="fasteignasala-home"),
     path('starfsmenn/', views.starfsmenn, name="starfsmenn-home"),
-    path('um_okkur/', views.um_okkur, name="um_okkur-home"),
+    path('um_okkur/', views.um_okkur, name="um-okkur-home"),
     path('soluskra/', views.soluskra, name="soluskra-home"),
     path('notandi/', include('notandi.urls')),
     path('<int:id>', views.get_apartm_by_id, name='apartment_details'),
--- project/fasteignasala/views.py
+++ project/fasteignasala/views.py
@@ -47,6 +47,37 @@ def home(request, query=None):
     context = {"apartments" : Apartment.objects.all()}
     return render(request, 'forsida/home.html', context)
 
+def soluskra(request, query=None):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET.get('search_filter')
+        min_mkr = request.GET.get('min_mkr')
+        max_mkr = request.GET.get('max_mkr')
+        min_size = request.GET.get('min_size')
+        max_size = request.GET.get('max_size')
+        min_rooms = request.GET.get('min_rooms')
+        max_rooms = request.GET.get('max_rooms')
+        apartments = [{
+            'id': x.id,
+            'address': x.address,
+            'price': x.price,
+            'fire_insurance': x.fire_insurance,
+            'estimated_value': x.estimated_value,
+            'type': x.type,
+            'size': x.size,
+            'num_rooms': x.num_rooms,
+            'num_bed_room': x.num_bed_room,
+            'num_bath_room': x.num_bath_room,
+            'description': x.description,
+            'town': x.town,
+            'zip': x.zip,
+            'first_image': x.apartmentimage_set.first().image
+        } for x in Apartment.objects.filter(address__icontains=search_filter).filter(price__gte=min_mkr, price__lte=max_mkr).
+        filter(num_rooms__gte=min_rooms, num_rooms__lte=max_rooms).filter(size__gte=min_size, size__lte=max_size)]
+        return JsonResponse({ 'data': apartments })
+
+    context = {"apartments": Apartment.objects.all()}
+    return render(request, 'soluskra/soluskra.html', context, {"title": "Söluskrá"})
+
 def get_apartm_by_id(request, id):
     return render(request, 'hus/husnaedi_details.html', {
     'apartment': get_object_or_404(Apartment, pk=id)
@@ -97,5 +128,3 @@ def um_okkur(request):
 def starfsmenn(request):
     return render(request, 'starfsmenn/starfsmenn.html', {"title": "Starfsmenn"})
 
-def soluskra(request):
-    return render(request, 'soluskra/soluskra.html', {"title": "Söluskrá"})
