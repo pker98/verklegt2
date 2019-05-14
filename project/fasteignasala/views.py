5797eccd4cfe86bd022a5a409674cf5068c44b7c
Matthías Finnur <matthias18@ru.is>
2019-05-14 06:19:09 +0000

    search filter completed 90%

--- project/fasteignasala/views.py
+++ project/fasteignasala/views.py
@@ -1,14 +1,22 @@
+import json
 from django.http import JsonResponse
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse
 from fasteignasala.forms.husnaediform import HusnaediCreateForm, HusnaediUpdateForm
 from fasteignasala.models import Apartment, ApartmentImage
+from django.template import loader, RequestContext
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required, permission_required
 
-def home(request):
+def home(request, query=None):
     if 'search_filter' in request.GET:
-        search_filter = request.GET['search_filter']
+        search_filter = request.GET.get('search_filter')
+        min_mkr = request.GET.get('min_mkr')
+        max_mkr = request.GET.get('max_mkr')
+        min_size = request.GET.get('min_size')
+        max_size = request.GET.get('max_size')
+        min_rooms = request.GET.get('min_rooms')
+        max_rooms = request.GET.get('max_rooms')
         apartments = [{
             'id': x.id,
             'address': x.address,
@@ -24,8 +32,18 @@ def home(request):
             'town': x.town,
             'zip': x.zip,
             'first_image': x.apartmentimage_set.first().image
-        } for x in Apartment.objects.filter(address__icontains=search_filter).order_by('address')]
+        } for x in Apartment.objects.filter(address__icontains=search_filter).filter(price__gte=min_mkr, price__lte=max_mkr).
+        filter(num_rooms__gte=min_rooms, num_rooms__lte=max_rooms).filter(size__gte=min_size, size__lte=max_size)]
         return JsonResponse({ 'data': apartments })
+
+    #sortval = request.REQUEST.get['sortval']
+    #results = Apartment.objects.filter(name__icontains=query).order_by(sortval)
+
+    #if request.is_ajax():
+        #t = loader.get_template('search_results.html')
+        #html = t.render(RequestContext({'results': results})
+        #return HttpResponse(json.dumps({'html': html}))
+
     context = {"apartments" : Apartment.objects.all()}
     return render(request, 'forsida/home.html', context)
 
