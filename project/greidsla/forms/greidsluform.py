78dbee1dbe856825a1ca280dcefaab7502108f4f
Matthías Finnur <matthias18@ru.is>
2019-05-15 16:26:55 +0000

    merge

--- project/greidsla/forms/greidsluform.py
+++ project/greidsla/forms/greidsluform.py
@@ -8,9 +8,16 @@ class GreidsluferliForm(forms.ModelForm):
         exclude = ['id']
         widgets = {
             'name': widgets.TextInput(attrs={ 'class': 'form-control' }),
-            'cardnumber': widgets.NumberInput(attrs={ 'class': 'form-control' }),
-            'ccv': widgets.NumberInput(attrs={ 'class': 'form-control'}),
-            'date_of_expiration': widgets.SelectDateWidget(empty_label=("Ár", "Mán"))
+            'ssn': widgets.NumberInput(attr={ 'class': 'form-control'}),
+            'address': widgets.TextInput(attrs={'class': 'form-control'}),
+            'zip': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'city': widgets.TextInput(attrs={'class': 'form-control'}),
+            'country': widgets.Select(choices={('Einbýlishús', 'Einbýlishús'),('Tvíbýlishús','Tvíbýlishús'),('Fjölbýlishús', 'Fjölbýlishús')}),
+            'phone': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'pay_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'card_number': widgets.NumberInput(attrs={ 'class': 'form-control' }),
+            'date_of_expiration': widgets.SelectDateWidget(empty_label=("Ár", "Mán")),
+            'ccv': widgets.NumberInput(attrs={ 'class': 'form-control'})
         }
         labels={
             'name': 'Korthafi',
@@ -18,3 +25,4 @@ class GreidsluferliForm(forms.ModelForm):
             'ccv': 'Öryggisnúmer (CCV)',
             'date_of_expiration': 'Gildistími'
         }
+
--- project/greidsla/views.py
+++ project/greidsla/views.py
@@ -7,14 +7,14 @@ from notandi.models import Profile
 
 @login_required
 def greidsla(request, id):
-    profile = get_object_or_404(Profile, pk=request.user.id)
+    profile = Profile.objects.filter(user=request.user).first()
     apartment = get_object_or_404(Apartment, pk=id)
     context = { 'profile': profile, 'apartment': apartment}
     return render(request, 'greidsluferli/greidsluferli.html', context)
 
 @login_required
 def stadfesting(request, id):
-    profile = get_object_or_404(Profile, pk=request.user.id)
+    profile = Profile.objects.filter(user=request.user).first()
     apartment = get_object_or_404(Apartment, pk=id)
     context = { 'profile': profile, 'apartment': apartment}
     return render(request, 'greidsluferli/stadfesting.html', context)
