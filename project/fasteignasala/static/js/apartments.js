da05635f9504363d96c259eb14f069589c3240d2
davidfannar98 <davidfannar98@gmail.com>
2019-05-15 15:31:59 +0000

    css styling again

--- project/fasteignasala/static/js/apartments.js
+++ project/fasteignasala/static/js/apartments.js
@@ -81,8 +81,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  //slides[slideIndex-1].style.display = "block";
+  //dots[slideIndex-1].className += " active";
 }
 
 $(document).ready(function() {
@@ -135,4 +135,9 @@ $(document).ready(function() {
             }
         })
     });
+});
+
+$("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#apartment_list").offset().top});
 });
