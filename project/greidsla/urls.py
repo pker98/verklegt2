245b2ccdc8dfaa7e8753357128554ecfd71c5be7
Matthías Finnur <matthias18@ru.is>
2019-05-16 14:35:23 +0000

    merge

--- project/greidsla/urls.py
+++ project/greidsla/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('greidsluferli/<int:id>', views.greidsla, name='greidsla'),
+    path('yfirlit/<int:id>', views.yfirlit, name='yfirlit'),
     path('stadfesting/<int:id>', views.stadfesting, name='stadfesting')
 ]
--- project/greidsla/views.py
+++ project/greidsla/views.py
@@ -10,14 +10,21 @@ def greidsla(request, id):
         payment_form = GreidsluferliForm(data=request.POST)
         if payment_form.is_valid():
             payment_form.save()
-            return redirect('stadfesting', id=id)
+            return redirect('yfirlit', id=id)
     else:
         payment_form = GreidsluferliForm()
     context = {'payment_form': payment_form, 'id': id}
     return render(request, 'greidsluferli/greidsluupplysingar.html', context)
 
 @login_required
+def yfirlit(request, id):
+    apartment = get_object_or_404(Apartment, pk=id)
+    buyer_info = get_object_or_404(Buyer, pk=request.user.id)
+    context = { 'apartment': apartment}
+    return render(request, 'greidsluferli/greidsluferliyfirlit.html'. context)
+
+@login_required
 def stadfesting(request, id):
     apartment = get_object_or_404(Apartment, pk=id)
     context = { 'apartment': apartment}
-    return render(request, 'greidsluferli/stadfesting.html', context)
+    return render(request, 'greidsluferli/greidslustadfesting.html', context)
